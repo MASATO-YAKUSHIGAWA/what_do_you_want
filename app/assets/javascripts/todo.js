13fa2d62ccd003ce5fc21ec68eda348c14eadac8
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-09-03 16:55:38 +0900

    fix

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -23,9 +23,9 @@ return html;
       data: {"todo[content]":$('#todo_content').val()}, //入力された値の取得 {'name':&('#id').val()}
       dataType: 'json',
     })
-    .done(function(data) {
+    .done(function(data) { //jbuilderからデータを受信
       console.log("成功");
-      $(".todo_form").val("")
+      $(".todo_form").val("") //text_areaを空白へ
       var html = buildHTML(data)
       $(".todo_content__tab_wrap__todo_area__todo_box__display").append(html)
      })
@@ -34,6 +34,7 @@ return html;
     });  
   })
 
+  // 削除機能
   $(document).on("click", ".todo_content__tab_wrap__todo_area__todo_box__display__one_todo_wrap__delete_btn",function(){
     var todo_id = $(this).data("todo-id")
     console.log(todo_id)
@@ -42,11 +43,11 @@ return html;
       type: 'DELETE',
       dataType: 'json',
       url: `/todos/${todo_id}`,
-      data: ({"id": todo_id}),
+      data: ({"id": todo_id}), //idを渡す
     })
     .done(function(){
       console.log("成功")
-      $(`[data-todo-id="${todo_id}"]`).remove()
+      $(`[data-todo-id="${todo_id}"]`).remove() //該当カスタムデータをremove
     })
     .fail(function(){
       console.log("失敗")
